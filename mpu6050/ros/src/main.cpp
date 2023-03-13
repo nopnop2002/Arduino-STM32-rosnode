35d18d09df286f84b79cd8c7ca10da85c24dfbc3
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-03-13 13:54:17 +0900

    refactored

--- mpu6050/ros/src/main.cpp
+++ mpu6050/ros/src/main.cpp
@@ -12,11 +12,8 @@ geometry_msgs::PoseStamped pose;
 ros::Publisher chatter("chatter", &str_msg);
 ros::Publisher pubpose("pose", &pose);
 
-
 // MPU control/status vars
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
 // orientation/motion vars
@@ -24,29 +21,18 @@ Quaternion q;           // [w, x, y, z]         quaternion container
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [roll, pitch, yaw]   roll/pitch/yaw container and gravity vector
 
+// chatter stuff
+char buffer[64];
+char wk[10];
+
 void setupMPU() {
-  char buffer[64];
   Wire.begin();
   Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
   mpu.initialize();
-  devStatus = mpu.dmpInitialize();
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXAccelOffset(-2889);
-  mpu.setYAccelOffset(-444);
-  mpu.setZAccelOffset(698);
-  mpu.setXGyroOffset(149);
-  mpu.setYGyroOffset(27);
-  mpu.setZGyroOffset(17);
 
   // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-    // Calibration Time: generate offsets and calibrate our MPU6050
-    mpu.CalibrateAccel(6);
-    mpu.CalibrateGyro(6);
-    mpu.setDMPEnabled(true);
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
+  devStatus = mpu.dmpInitialize();
+  if (devStatus != 0) {
     //Serial.print("DMP Initialization failed.");
     strcpy(buffer, "DMP Initialization failed.");
     str_msg.data = buffer;
@@ -54,77 +40,82 @@ void setupMPU() {
     nh.spinOnce();
     while(1) {}
   }
+
+  // supply your own offsets here, scaled for min sensitivity
+  mpu.setXAccelOffset(-2889);
+  mpu.setYAccelOffset(-444);
+  mpu.setZAccelOffset(698);
+  mpu.setXGyroOffset(149);
+  mpu.setYGyroOffset(27);
+  mpu.setZGyroOffset(17);
+
+  // Calibration Time: generate offsets and calibrate our MPU6050
+  mpu.CalibrateAccel(6);
+  mpu.CalibrateGyro(6);
+  mpu.setDMPEnabled(true);
 }
 
 void getYawPitchRoll() {
-  char buffer[64];
-  char wk[10];
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    //Serial.print(ypr[2] * 180 / M_PI);
-    //Serial.print(",");
-    //Serial.print(ypr[1] * 180 / M_PI);
-    //Serial.print(",");
-    //Serial.println(ypr[0] * 180 / M_PI);
-    float roll = ypr[2] * 180 / M_PI;
-    float pitch = ypr[1] * 180 / M_PI;
-    float yaw = ypr[0] * 180 / M_PI;
-    strcpy(buffer, "roll:");
-    dtostrf(roll, 6, 2, wk);
-    strcat(buffer, wk);
-    strcat(buffer, " pitch:");
-    dtostrf(pitch, 6, 2, wk);
-    strcat(buffer, wk);
-    strcat(buffer, " yaw:");
-    dtostrf(yaw, 6, 2, wk);
-    strcat(buffer, wk);
-    //Serial.println(buffer);
-    str_msg.data = buffer;
-    chatter.publish( &str_msg );
-  }
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetGravity(&gravity, &q);
+  mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+  //Serial.print(ypr[2] * 180 / M_PI);
+  //Serial.print(",");
+  //Serial.print(ypr[1] * 180 / M_PI);
+  //Serial.print(",");
+  //Serial.println(ypr[0] * 180 / M_PI);
+  float roll = ypr[2] * 180 / M_PI;
+  float pitch = ypr[1] * 180 / M_PI;
+  float yaw = ypr[0] * 180 / M_PI;
+  strcpy(buffer, "roll:");
+  dtostrf(roll, 6, 2, wk);
+  strcat(buffer, wk);
+  strcat(buffer, " pitch:");
+  dtostrf(pitch, 6, 2, wk);
+  strcat(buffer, wk);
+  strcat(buffer, " yaw:");
+  dtostrf(yaw, 6, 2, wk);
+  strcat(buffer, wk);
+  //Serial.println(buffer);
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
 }
 
 //char frame_id[] = "mpu6050";
 char frame_id[] = "map";
 
 void getQuaternion() {
-  char buffer[64];
-  char wk[10];
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    //Serial.print(q.x);
-    //Serial.print(",");
-    //Serial.print(q.y);
-    //Serial.print(",");
-    //Serial.print(q.z);
-    //Serial.print(",");
-    //Serial.println(q.w);
-    strcpy(buffer, "x:");
-    dtostrf(q.x, 6, 2, wk);
-    strcat(buffer, wk);
-    strcat(buffer, " y:");
-    dtostrf(q.y, 6, 2, wk);
-    strcat(buffer, wk);
-    strcat(buffer, " z:");
-    dtostrf(q.z, 6, 2, wk);
-    strcat(buffer, wk);
-    strcat(buffer, " w:");
-    dtostrf(q.w, 6, 2, wk);
-    strcat(buffer, wk);
-    //Serial.println(buffer);
-    str_msg.data = buffer;
-    chatter.publish( &str_msg );
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  //Serial.print(q.x);
+  //Serial.print(",");
+  //Serial.print(q.y);
+  //Serial.print(",");
+  //Serial.print(q.z);
+  //Serial.print(",");
+  //Serial.println(q.w);
+  strcpy(buffer, "x:");
+  dtostrf(q.x, 6, 2, wk);
+  strcat(buffer, wk);
+  strcat(buffer, " y:");
+  dtostrf(q.y, 6, 2, wk);
+  strcat(buffer, wk);
+  strcat(buffer, " z:");
+  dtostrf(q.z, 6, 2, wk);
+  strcat(buffer, wk);
+  strcat(buffer, " w:");
+  dtostrf(q.w, 6, 2, wk);
+  strcat(buffer, wk);
+  //Serial.println(buffer);
+  str_msg.data = buffer;
+  chatter.publish( &str_msg );
 
-    pose.header.stamp = nh.now();
-    pose.header.frame_id = frame_id;
-    pose.pose.orientation.x = q.x;
-    pose.pose.orientation.y = q.y;
-    pose.pose.orientation.z = q.z;
-    pose.pose.orientation.w = q.w;
-    pubpose.publish( &pose );
-  }
+  pose.header.stamp = nh.now();
+  pose.header.frame_id = frame_id;
+  pose.pose.orientation.x = q.x;
+  pose.pose.orientation.y = q.y;
+  pose.pose.orientation.z = q.z;
+  pose.pose.orientation.w = q.w;
+  pubpose.publish( &pose );
 }
 
 void setup() {
@@ -137,8 +128,10 @@ void setup() {
 }
 
 void loop() {
-  //getYawPitchRoll();
-  getQuaternion();
-  nh.spinOnce();
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+    getYawPitchRoll();
+    getQuaternion();
+    nh.spinOnce();
+  }
   delay(100);
 }
